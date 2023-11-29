f908d634a6c72a51aa1ad69acb5c767b220383a9
GabrielRebes <149078383+GabrielRebes@users.noreply.github.com>
2023-11-29 17:14:27 -0300

    pt

--- src/index.py
+++ src/index.py
@@ -8,14 +8,14 @@ async def root():
     return {"message": "Hello World"}
 
 # token => https://en.wikipedia.org/wiki/JSON_Web_Token
-@app.get("/partida/{id_partida}/tabuleiro")
-async def get_tabuleiro(token, id_partida):
-    pass
+#@app.get("/partida/{id_partida}/tabuleiro")
+#async def get_tabuleiro(token, id_partida):
+#    pass
 
-@app.get("/")
-async def lista_jogadores():
-    return JogadorController.get_instance().lista_todos_os_jogadores()
+#@app.get("/")
+#async def lista_jogadores():
+#    return JogadorController.get_instance().lista_todos_os_jogadores()
 
-@app.get("/top3")
-async def ranking_top3():
-    return JogadorController.get_instance().lista_ranking_top_3()
+#@app.get("/top3")
+#async def ranking_top3():
+#    return JogadorController.get_instance().lista_ranking_top_3()
