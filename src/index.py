df1dc8e6e3d3d9642e7650d32ea00cccd408364f
GabrielRebes <149078383+GabrielRebes@users.noreply.github.com>
2023-11-30 14:52:54 -0300

    Update index.py

--- src/index.py
+++ src/index.py
@@ -3,19 +3,19 @@ from batalha_naval.controllers.jogador_controller import JogadorController
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+#@app.get("/")
+#async def root():
+#   return {"message": "Hello World"}
 
 # token => https://en.wikipedia.org/wiki/JSON_Web_Token
-@app.get("/partida/{id_partida}/tabuleiro")
-async def get_tabuleiro(token, id_partida):
-    pass
+#@app.get("/partida/{id_partida}/tabuleiro")
+#async def get_tabuleiro(token, id_partida):
+#    pass
 
-@app.get("/")
-async def lista_jogadores():
-    return JogadorController.get_instance().lista_todos_os_jogadores()
+#@app.get("/")
+#async def lista_jogadores():
+#    return JogadorController.get_instance().lista_todos_os_jogadores()
 
-@app.get("/top3")
-async def ranking_top3():
-    return JogadorController.get_instance().lista_ranking_top_3()
+#@app.get("/top3")
+#async def ranking_top3():
+#    return JogadorController.get_instance().lista_ranking_top_3()
